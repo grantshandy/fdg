e7ffae63eca3223e153a9da58909eb4f29c04732
Aditya Suresh <ethanaditya@gmail.com>
2022-04-17 16:46:02 -0600

    slugs

--- examples/macroquad-basic.rs
+++ examples/macroquad-basic.rs
@@ -11,9 +11,15 @@ async fn main() {
 
     let one = graph.add_force_node("One", ());
     let two = graph.add_force_node("Two", ());
-    let _three = graph.add_force_node("Three", ());
+    let three = graph.add_force_node("Three", ());
 
     graph.add_edge(one, two, ());
+    graph.add_edge(two, three, ());
+    graph.add_edge(three, one, ());
+
+
+    let two = graph.add_force_node("", ());
+    let three = graph.add_force_node("", ());
 
     let mut sim = Simulation::from_graph(graph, Dimensions::Two, SimulationParameters::default());
 
--- examples/macroquad-max-nodes.rs
+++ examples/macroquad-max-nodes.rs
@@ -3,14 +3,21 @@ use fdg::{
     Dimensions, Simulation, SimulationParameters,
 };
 
+use rand::Rng; 
+
 #[macroquad::main("Force Graph Max Nodes Demo")]
 async fn main() {
     pretty_env_logger::init();
 
     let mut graph: ForceGraph<()> = ForceGraph::default();
 
-    for i in 0..700 {
-        graph.add_force_node(i.to_string(), ());
+    for i in 0..75 {
+        let node = graph.add_force_node(i.to_string(), ());
+        let node = &mut graph[node];
+        let mut rng = rand::thread_rng();
+        
+        node.mass = rng.gen_range(0.0..10.0);
+
     }
 
     let mut sim = Simulation::from_graph(graph, Dimensions::Two, SimulationParameters::default());
