b5b52275eaeba34a54733e4ea05aabf54d1792a3
grantshandy <grantshandy@gmail.com>
2022-09-06 14:04:37 -0600

    try to fix ci and build script

--- build-web-examples.sh
+++ build-web-examples.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rustup target add wasm32-unknown-unknown
 
-cargo build --release --target wasm32-unknown-unknown --examples
+cd fdg-macroquad && cargo build --release --target wasm32-unknown-unknown --examples && cd ..
 
 cp target/wasm32-unknown-unknown/release/examples/view_nd_cube.wasm ./fdg-macroquad/examples/web-examples/
 cp target/wasm32-unknown-unknown/release/examples/view_binary_tree.wasm ./fdg-macroquad/examples/web-examples/
