bc4f66ed6a28da1b824a052f3574f022998402c3
grantshandy <grantshandy@users.noreply.github.com>
2022-09-10 00:18:56 +0000

    deploy: fef7414dcc26fab856b1529367301f9b00b25708

--- fdg-img/pkg/fdg-img_bg.wasm.d.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function generate_svg(a: number, b: number, c: number, d: number): void;
-export function __wbindgen_malloc(a: number): number;
-export function __wbindgen_realloc(a: number, b: number, c: number): number;
-export function __wbindgen_add_to_stack_pointer(a: number): number;
-export function __wbindgen_free(a: number, b: number): void;
-export function __wbindgen_exn_store(a: number): void;
